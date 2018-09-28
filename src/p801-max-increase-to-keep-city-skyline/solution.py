fc76ace613ae542cad121b21436b7946623ccc5f
Basim Ramadhan <basim.ramadhan@gmail.com>
2018-09-28 19:30:13 -0400

    Problem 801: solved in O(n^2)

--- src/p801-max-increase-to-keep-city-skyline/solution.py
+++ src/p801-max-increase-to-keep-city-skyline/solution.py
@@ -5,4 +5,14 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
-        return 0
+        transpose = lambda a: [[a[c][r] for c in range(len(a))] for r in range(len(a))]
+        length = width = len(grid)
+        row_maxs = [max(row) for row in grid]
+        col_maxs = [max(col) for col in transpose(grid)]
+        increase = 0
+        for i in range(length):
+            for j in range(width):
+                new_height = min(row_maxs[i], col_maxs[j])
+                curr_height = grid[i][j]
+                increase += (new_height - curr_height)
+        return increase
