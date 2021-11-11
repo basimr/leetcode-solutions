c70c971635eca0120dd84302febb85bf17d07c8b
Baz <basim.ramadhan@gmail.com>
2021-11-11 12:18:22 -0800

    Solve problem 35 using a recursive divide and conquer approach

--- src/p035-search-insert-position/solution.py
+++ src/p035-search-insert-position/solution.py
@@ -3,4 +3,25 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return 0
+        return self.helper(nums, target, 0, len(nums) - 1)
+
+    def helper(self, nums: List[int], target: int, start: int, end: int):
+        if start == end:
+            if target <= nums[start]:
+                return start
+            else:
+                return start + 1
+        if target < nums[start]:
+            return start
+        if target > nums[end]:
+            return end + 1
+
+        mid_i = (start + end) // 2
+        mid_num = nums[mid_i]
+
+        if target < mid_num:
+            return self.helper(nums, target, start, mid_i)
+        elif target > mid_num:
+            return self.helper(nums, target, mid_i + 1, end)
+        else:
+            return mid_i
--- src/p035-search-insert-position/test_solution.py
+++ src/p035-search-insert-position/test_solution.py
@@ -67,6 +67,27 @@ class SolutionTestCase(unittest.TestCase):
         output = Solution().searchInsert(nums, target)
         self.assertEqual(expected, output)
 
+    def test_insert_at_start_with_one_number(self):
+        nums = [1]
+        target = 0
+        expected = 0
+        output = Solution().searchInsert(nums, target)
+        self.assertEqual(expected, output)
+
+    def test_insert_at_end_with_one_number(self):
+        nums = [1]
+        target = 2
+        expected = 1
+        output = Solution().searchInsert(nums, target)
+        self.assertEqual(expected, output)
+
+    def test_insert_at_start_with_same_number(self):
+        nums = [1]
+        target = 1
+        expected = 0
+        output = Solution().searchInsert(nums, target)
+        self.assertEqual(expected, output)
+
 
 if __name__ == '__main__':
     unittest.main()
